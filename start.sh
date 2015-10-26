1066c1bfe64e5d443fbfa797e837e3faefaefe8d
root <root@ts13.freets3.net>
2015-10-26 18:01:56 -0400

    Initial Commit

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-until ./ts3server_minimal_runscript.sh; do
+cd /home/ts
+./AccountingServerEmulator-Linux
+ulimit -n 65535
+until ./ts3server_minimal_runscript.sh dbconnections=35 voice_ip=${BOX_IP} filetransfer_ip=${BOX_IP} dbclientkeepdays=7 dblogkeepdays=7 logquerycommands=1; do
 echo 'Server crashed'
 sleep 1
 done
