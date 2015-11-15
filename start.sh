82a92bd687566bc549def5d261a889140d1b5274
ryanbentley <ryan@ryanbentley.co>
2015-11-15 21:03:46 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-cd /home/ts
+cd /home/freets3-server
 ./AccountingServerEmulator-Linux
 ulimit -n 65535
 until ./ts3server_minimal_runscript.sh dbconnections=35 voice_ip=${BOX_IP} filetransfer_ip=${BOX_IP} dbclientkeepdays=7 dblogkeepdays=7 logquerycommands=1; do
